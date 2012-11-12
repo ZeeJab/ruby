45a3ff9a652766a5422af32453dc9af0c0336311
Zahra Jabini <zahra.arch@gmail.com>
2012-11-12 10:10:09 -0500

    quiz 10 done.

--- quizzes/quiz10.rb
+++ quizzes/quiz10.rb
@@ -3,3 +3,19 @@
 # response.explode
 # your output will be the square of 10 is 100
 # make it so there's no error message
+
+puts "enter a (n)umber or (q)uit"
+response = gets.to_f
+
+while response != 'q'
+
+	begin 
+	response.explode
+	rescue 
+		puts "The square of #{response} is #{response*response}! Good job."
+
+	end
+
+	puts "(n)umber or (q)uit"
+	response = gets.chomp
+end
